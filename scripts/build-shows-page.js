b6923f7fdf54336db562d207803e885a49dd7c90
Nicholas Zambrano <nicholaszambrano@hotmail.co.uk>
2023-07-16 19:53:51 +0100

    added the top headers and removed all the other h3 tags

--- scripts/build-shows-page.js
+++ scripts/build-shows-page.js
@@ -32,42 +32,6 @@ const shows = [
   },
 ];
 
-
-//  i appended each tag within each article
-// const containerEl = document.querySelector(".shows__container");
-// // this iteratng through each 'date' value
-// for(let i=0; i<shows.length;i++){
-//   const articleShowEl = document.createElement("article");
-//   containerEl.appendChild(articleShowEl); // appended an article, which will contain the labels
-//   articleShowEl.classList.add("shows__wrapper");
-
-//   const dateTagEl = document.createElement("label");
-//   dateTagEl.classList.add("shows__date")
-//   // modifying the text, so that it appears on the website
-//   dateTagEl.innerText = shows[i].date ; // printing the value i.e the date
-
-//   // creating a venue label
-//   const venueTagEl = document.createElement("label");
-//   venueTagEl.innerText = shows[i].venue;
-
-//   const locationTagEl = document.createElement("label");
-//   locationTagEl.innerText = shows[i].location;
-
-//   articleShowEl.appendChild(dateTagEl);
-//   articleShowEl.appendChild(venueTagEl);
-//   articleShowEl.appendChild(locationTagEl);
-//   // console.log(dateTagEl);
-//   // console.log(venueTagEl);
-
-// }
-
-//  loop over the array
-// create a article, which represents a show
-//(need more tags: create a p tag i.e date label
-//repeat for all tags( date value, venue label...)
-// For each of those tags, add them to the article
-// at the end, append the article , to the .show_container
-
 // selected the 'shows__container'
 const containerEl = document.querySelector(".shows__container");
 // console.log(containerEl);
@@ -85,11 +49,6 @@ const fill_shows = (show) => {
   section.classList.add("shows__wrapper");
 
   // THIS IS FOR THE UNORDERED LIST:
-  
-  // const unorderedListEl = document.createElement("ul");
-  // create class name: 
-  // unorderedListEl.classList.add("shows__items");
-  // div.appendChild(unorderedListEl)
 
   // going to create 3 small divs:
   const divDate = document.createElement("div");
@@ -114,12 +73,11 @@ const fill_shows = (show) => {
   // now adding the date of the show:
   const yearEl = document.createElement("p");
   // add a class:
-  yearEl.classList.add("shows__year","shows__pTag");
+  yearEl.classList.add("shows__year", "shows__pTag");
   // now assigning the year(modifying the text) to the value of the key 'date'
   yearEl.innerText = show.date;
   divDate.appendChild(yearEl); // appending the label to the small div
   // unorderedListEl.append(yearEl);
-
 
   const h3VenueEl = document.createElement("h3");
   h3VenueEl.classList.add("shows__h3");
@@ -128,13 +86,12 @@ const fill_shows = (show) => {
   divVenue.appendChild(h3VenueEl);
   // unorderedListEl.append(h3VenueEl);
 
-
   // now create the venue from the objects
 
   // create the element
   const venueTagEl = document.createElement("p");
   // add a class to the element,also added a common class 'shows__label'
-  venueTagEl.classList.add("shows__venue","shows__pTag");
+  venueTagEl.classList.add("shows__venue", "shows__pTag");
   // then modify the created element
   venueTagEl.innerText = show.venue;
   // now append the venue value to the div:
@@ -163,31 +120,23 @@ const fill_shows = (show) => {
   section.appendChild(divVenue);
   section.appendChild(divLocation);
 
-
   const buttonWrapperEl = document.createElement("div");
-  buttonWrapperEl.classList.add("shows__button-wrapper")
+  buttonWrapperEl.classList.add("shows__button-wrapper");
 
   // now create the button:
   const buttonEl = document.createElement("button");
   buttonEl.classList.add("shows__button");
-  buttonEl.innerText ="BUY TICKETS"
+  buttonEl.innerText = "BUY TICKETS";
   buttonWrapperEl.append(buttonEl); // appended the button to the button wrapper
-
 
   // add the wrapper div to the article
   article.appendChild(section);
 
   // then addin the button wrapper underneath
-  article.append(buttonWrapperEl)
-
-
+  article.append(buttonWrapperEl);
 
   // now add the article to the DOM
   containerEl.appendChild(article);
-
-
-
-
 };
 
 for (let i = 0; i < shows.length; i++) {
@@ -196,8 +145,26 @@ for (let i = 0; i < shows.length; i++) {
   // and call the function within the loop
   fill_shows(show);
   // console.log(show);
-
-  
 }
-// console.log(fill_shows(shows[0]));
-// 
+
+// create a div 
+const topDiv = document.createElement("div");
+topDiv.classList.add("shows__topWrapper")
+
+const topHeaderDate = document.createElement("h3");
+topHeaderDate.classList.add("shows__topH3")
+topHeaderDate.innerText = "DATE";
+topDiv.appendChild(topHeaderDate);
+
+const topHeaderVenue = document.createElement("h3");
+topHeaderVenue.classList.add("shows__topH3","shows__topH3--venue"); // added a modifier
+topHeaderVenue.innerText ="VENUE";
+topDiv.appendChild(topHeaderVenue);
+
+const topHeaderLocation = document.createElement("h3");
+topHeaderLocation.classList.add("shows__topH3", "shows__topH3--location")
+topHeaderLocation.innerHTML = "LOCATION";
+topDiv.appendChild(topHeaderLocation);
+
+containerEl.insertBefore(topDiv,containerEl.firstChild)
+
